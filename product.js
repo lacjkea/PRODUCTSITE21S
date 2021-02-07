d5056745ddcd4e7b9de1fce0436d783fa19ae98e
lacj@kea.dk <lacj@kea.dk>
2021-02-07 21:46:17 +0100

    product.html partly dynamic (needs more work)

---
+++ product.js
@@ -0,0 +1,26 @@
+//console.log("hello");
+const id = 2801; //product ID
+const url = "https://kea-alt-del.dk/t7/api/products/" + id; //jofh: 2801
+//1. fetch Data
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+//2. populate the page
+function showProduct(product) {
+  console.table(product);
+  //   document.querySelector("#main-nav ul li:nth-child(2) a").textContent = product;
+  document.querySelector("#main-nav ul li:nth-child(3) a").textContent =
+    product.brandname;
+  const displayName = product.productdisplayname;
+  document.querySelector(
+    "#main-nav ul li:nth-child(4) a"
+  ).textContent = displayName;
+
+  //const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  const imgEl = document.querySelector(".center img");
+  imgEl.src = imagePath;
+  imgEl.alt = displayName;
+}
